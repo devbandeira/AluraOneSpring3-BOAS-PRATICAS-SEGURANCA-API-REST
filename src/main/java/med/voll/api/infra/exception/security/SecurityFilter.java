0db64c04ee31d4212a6153c40d3f17ff24c741c0
devbandeira <dev.bandeira@outlook.com>
2023-09-30 17:44:56 -0300

    Recuperando o token

--- src/main/java/med/voll/api/infra/exception/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/exception/security/SecurityFilter.java
@@ -8,31 +8,33 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component/*n é serviço, controller, repositorio, configuração.*/
-/*nesse cenário onde a classe não tem um tipo especifico usamos o @Component, é utilizado para que o Spring carregue
-* uma classe/componente genérico*/
+@Component
 public class SecurityFilter extends OncePerRequestFilter {
-    /*Estamos extendendo a classe OncePerRequestFilter do SPring e ela implementa a interface Filter do jakarta.*/
 
-    /*Quando chegar uma requisição na api o spring sabe desse filter, e ele chama o método doFilterInternal, que vai ser executado
-    * apenas uma vez por REQUEST*/
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        /*Até aqui ele intercepta, chama este filtro, porém não faz mais nada além disso.
-        * Preciso dizer o que ele vai chamar como próximo, para seguir o fluxo da requisição
-        * */
-        /*Este método recebe 3 parâmetros
-        * request -> Pega coisas da requisição
-        * response -> Enviar coisas na resposta
-        * filterChain -> Representa a cadeira de filtros que existem na aplicação.
-        *
-        * para eu chamar o próximo, devo usar ele.*/
 
-        filterChain.doFilter(request, response);/*para continuar o fluxo da requisição e ele encaminhar par ao proximo
-        filtro o request e o responde*/
+        /*logica para recuperar o TOKEN*/
+        /*O envio de um token é realizado em um cabeçalho HTTP, chamado Authorization, enviado pelo front end*/
 
+        var tokenJWT = recuperarToken(request); /*passo o request pq o cabeçalho ta associado a ele*/
+
+        System.out.println(tokenJWT);/*para ver se vamos conseguir recuperar o token. R.: Bearer é o prefixo padrão utilizado
+        para token JWT, posso mudar o prefixo no insomminia*/
+
+        /* ********************* */
+
+        filterChain.doFilter(request, response);
     }
 
+    /*método criado somente para recuperar o token*/
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization"); /*Authorization -> String paga pegar o Authorization do cabeçalho*/
+        if(authorizationHeader == null){
+            throw  new RuntimeException("Token JWT não enviado no cabeçado Authorization");
+        }
+        return authorizationHeader.replace("Bearer ", "");/*Tirando o prefixo bearer do cabeçalho*/
+    }
 
 }
 
